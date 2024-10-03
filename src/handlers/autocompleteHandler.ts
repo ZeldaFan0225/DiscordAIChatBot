8d5223554a6e297b9bff940b6e48174dbcd7db8f
ZeldaFan0225 <78901316+ZeldaFan0225@users.noreply.github.com>
2024-10-03 20:42:43 +0200

    fix: autocomplete in private channels through user app

--- src/handlers/autocompleteHandler.ts
+++ src/handlers/autocompleteHandler.ts
@@ -8,8 +8,9 @@ export async function handleAutocomplete(interaction: AutocompleteInteraction, c
     const context = new AutocompleteContext({interaction, client})
     if(client.config.user_blacklist?.includes(interaction.user.id))
         return await context.error();
-    if(!interaction.channel)
-        return await context.error()
+    // the following, commented out lines broke functionality when using the bot as a user app in a channel the bot can't see
+    //if(!interaction.channel)
+    //    return await context.error()
     if(command.staff_only && !context.is_staff)
         return await context.error()
     return await command.autocomplete(context).catch(console.error)
